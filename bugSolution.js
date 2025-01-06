47b31e5b1dd1f6dcceb8839892a0117878f5c71b
BugHunter-X <askzenai@gmail.com>
2025-01-06 06:20:07 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Open up App.js to start working on your app!</Text>      {/*Added this to check if the issue still persists*/}
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+// Solution: Add necessary package installs and double-check package.json
